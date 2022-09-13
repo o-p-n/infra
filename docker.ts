124dcb51ab47434e3a539ca405429c70c9d819c4
Matthew A. Miller <linuxwolf@outer-planes.net>
2022-09-13 08:25:14 -0600

    Separate stacks for DigitalOcean vs Docker (#6)
    Uses separate stacks for digitalocean resources versus docker resources. This helps keep deployments fiddling-free.

---
+++ docker.ts
@@ -0,0 +1,21 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as docker from "@pulumi/docker";
+
+export function dockerStack(stack: string, config: pulumi.Config) {
+  const dockerProvider = new docker.Provider("docker", {
+    host: "ssh://admin@outer-planes.net",
+  });
+  const dockerOpts = {
+    provider: dockerProvider,
+  };
+  
+  const network = new docker.Network("public", {
+    name: "public",
+    driver: "overlay",
+    attachable: true,
+  }, dockerOpts);
+  
+  const certsVolume = new docker.Volume("certs", {
+    name: "certs",
+  }, dockerOpts);
+}
