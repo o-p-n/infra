18fb9b08da44a23b282e81868150c439d9e3d01a
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-01-25 18:18:56 -0700

    istio: move gateway api CRD to -system

--- modules/istio-system/k8s/bootstrap/apply-ready.sh
+++ modules/istio-system/k8s/bootstrap/apply-ready.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+kubectl --context=${ENV} \
+  wait --for=condition=Established \
+  crd gatewayclasses.gateway.networking.k8s.io
+
+kubectl --context=${ENV} \
+  wait --for=condition=Established \
+  crd gateways.gateway.networking.k8s.io
+
 kubectl --context=${ENV} --namespace=istio-system \
   wait --for=condition=ready pod \
     -l app=istiod
