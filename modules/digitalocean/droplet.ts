56112f208b452794c78d67874b0f6e581f93d118
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-02-15 09:05:20 -0700

    include humans in root droplet

--- modules/digitalocean/droplet.ts
+++ modules/digitalocean/droplet.ts
@@ -2,44 +2,53 @@ import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
 
 export function stack() {
-  const sshKeys = digitalocean.getSshKeysOutput() as pulumi.Output<digitalocean.GetSshKeysResult>;
+  const sshKeysOutput = digitalocean.getSshKeysOutput() as pulumi.Output<digitalocean.GetSshKeysResult>;
 
-  const userData = sshKeys.apply((result) => {
+  const userData = sshKeysOutput.apply((result) => {
       const botKeys = result.sshKeys.filter((ssh) => ssh.name.startsWith("bot-"))
-        .map((ssh) => `"${ssh.fingerprint}"`);
+        .map((ssh) => ssh.publicKey);
       const humanKeys = result.sshKeys.filter((ssh) => ssh.name.startsWith("human-"))
-        .map((ssh) => `"${ssh.fingerprint}"`);
+        .map((ssh) => ssh.publicKey);
 
       return `
 #cloud-config
+package_update: true
+package_upgrade: true
+snap:
+  commands:
+    - snap install microk8s --classic --channel=1.28
 users:
   - name: human
     lock-password: false
-    groups: sudo, microk8s
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    groups: microk8s, sudo
     shell: /bin/bash
-    sudo: ["ALL (ALL): NOPASSWD:ALL"]
-    ssh-authorized-keys: [${humanKeys.join(", ")}]
+    ssh_authorized_keys: [${humanKeys.join(", ")}]
   - name: bot
     lock-password: false
     groups: microk8s
     shell: /bin/bash
-    ssh-authorized-keys: [${botKeys.join(", ")}]
-runcmd:
-  - sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
-  - restart ssh
-  - snap install microk8s --classic --channel=1.28
-  - microk8s start
+    ssh_authorized_keys: [${botKeys.join(", ")}]
+ssh_genkeytypes: [ecdsa, ed25519]
+ssh_pwauth: false    
 `;
   });
+
+  const sshKeys = sshKeysOutput.apply(
+    (result) => result.sshKeys
+      .filter((ssh) => ssh.name.startsWith("human-"))
+      .map((k) => `${k.id}`)
+  );
 
   const instance = new digitalocean.Droplet("o-p.n", {
     name: "o-p.n",
     image: "ubuntu-22-04-x64",
-    region: "nyc3",
-    size: "s-2vcpu-2gb-amd",
+    region: "sfo3",
+    size: "s-4vcpu-8gb",
     ipv6: true,
     monitoring: true,
     userData: userData,
+    sshKeys,
   });
 
 
