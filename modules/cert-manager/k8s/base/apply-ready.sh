b1308642797f37dad0155441ad3bc578dc8c8523
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-10-09 21:03:18 -0600

    remove old

--- modules/cert-manager/k8s/base/apply-ready.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-kubectl wait --for=condition=ready clusterIssuer cert-manager
--- modules/kube-state-metrics/k8s/base/apply-ready.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-kubectl wait \
-  --namespace=kube-system \
-  --for=condition=ready \
-  pod \
-  -l app.kubernetes.io/name=kube-state-metrics
