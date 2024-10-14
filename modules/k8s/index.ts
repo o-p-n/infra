17f3feddcb46549954fff1de89eb49e0a745caa1
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-10-14 11:55:24 -0600

    cert-manager

--- modules/k8s/index.ts
+++ modules/k8s/index.ts
@@ -2,16 +2,19 @@ import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 import { getProvider } from "./_provider";
 
-import istioSystemStack from "./istio-system";
 import infraCoreStack from "./infra-core";
+import istioSystemStack from "./istio-system";
+import certManagerStack from "./cert-manager";
 
 export default async function stack() {
   const provider = await getProvider();
   const infraCore = await infraCoreStack(provider);
   const istioSystem = await istioSystemStack(provider);
+  const certManager = await certManagerStack(provider);
 
   return {
     infraCore,
     istioSystem,
+    certManager,
   };
 }
