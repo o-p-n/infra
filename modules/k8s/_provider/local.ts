47696938e2d81c8ff3443acd4f87923fe3d52bc1
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-10-14 11:55:06 -0600

    local: install minikube

--- modules/k8s/_provider/local.ts
+++ modules/k8s/_provider/local.ts
@@ -1,10 +1,19 @@
-import * as pulumi from "@pulumi/pulumi";
 import { local } from "@pulumi/command";
 
 export default async function stack() {
-  const runnit = new local.Command("local-kubeconfig", {
-    create: "cat ~/.kube/local.config",
+  const environment = {
+    KUBECONFIG: `${process.env["HOME"]}/.kube/local.config`,
+  };
+
+  const minikube = new local.Command("local-minikube", {
+    create: "minikube status || minikube start --cpus=2 --memory=8g --driver=docker --kubernetes-version=latest --insecure-registry=host.minikube.internal:5000",
+    delete: "minikube delete",
+    environment,
   });
+  const runnit = new local.Command("local-kubeconfig", {
+    create: "cat ${KUBECONFIG}",
+    environment,
+  }, { dependsOn: [minikube] });
 
   return {
     kubeconfig: runnit.stdout,
