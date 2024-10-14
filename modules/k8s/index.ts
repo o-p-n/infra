73e1ebc9813d395e04a7ac9c11b5e8b845847fc7
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-10-14 12:27:15 -0600

    metallb

--- modules/k8s/index.ts
+++ modules/k8s/index.ts
@@ -5,16 +5,24 @@ import { getProvider } from "./_provider";
 import infraCoreStack from "./infra-core";
 import istioSystemStack from "./istio-system";
 import certManagerStack from "./cert-manager";
+import metallbStack from "./metallb";
 
 export default async function stack() {
+  const stackName = pulumi.getStack();
+
   const provider = await getProvider();
   const infraCore = await infraCoreStack(provider);
   const istioSystem = await istioSystemStack(provider);
   const certManager = await certManagerStack(provider);
 
+  const metallb = (stackName !== "local") ?
+    await metallbStack(provider) :
+    pulumi.output(undefined);
+
   return {
     infraCore,
     istioSystem,
     certManager,
+    metallb,
   };
 }
---
+++ modules/k8s/metallb.ts
@@ -0,0 +1,32 @@
+import * as k8s from "@pulumi/kubernetes";
+import { outputs } from "./_util";
+
+const namespace = "metallb-system";
+const version = "0.14.8";
+
+export default async function metallbStack(provider: k8s.Provider) {
+  const ns = new k8s.core.v1.Namespace(namespace, {
+    metadata: {
+      name: namespace,
+      labels: {
+        "pod-security.kubernetes.io/enforce": "privileged",
+        "pod-security.kubernetes.io/audit": "privileged",
+        "pod-security.kubernetes.io/warn": "privileged",
+      }
+    },
+  }, { provider });
+
+  const metallb = new k8s.helm.v3.Release(namespace, {
+    chart: "metallb",
+    version,
+    namespace,
+    repositoryOpts: {
+      repo: "https://metallb.github.io/metallb",
+    },
+  });
+
+  return {
+    namespace,
+    metallb: outputs(metallb),
+  }
+}
