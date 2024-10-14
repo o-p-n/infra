3cbe502168a83869370595aa6d14814bf0a39b7f
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-10-14 15:03:29 -0600

    monitoring: turn on grafana

--- modules/k8s/monitoring/index.ts
+++ modules/k8s/monitoring/index.ts
@@ -1,18 +1,20 @@
+import { Config } from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
 const namespace = "monitoring";
 
+const config = new Config();
+
 export default async function stack(provider: k8s.Provider) {
+  const adminPassword = config.require("monitoring-admin-password");
+
   const ns = new k8s.core.v1.Namespace(namespace, {
     metadata: {
       name: namespace,
-      labels: {
-        "istio-injection": "enabled",
-      }
     },
   }, { provider });
 
-  const prometheus = new k8s.helm.v3.Release(`${namespace}-prometheus`, {
+  const prometheus = new k8s.helm.v3.Release(`${namespace}`, {
     chart: "kube-prometheus-stack",
     version: "65.2.0",
     namespace,
@@ -20,7 +22,12 @@ export default async function stack(provider: k8s.Provider) {
       repo: "https://prometheus-community.github.io/helm-charts",
     },
     values: {
-      granana: { enabled: false },
+      grafana: {
+        adminPassword,
+        podLabels: {
+          "sidecar.istio.io/inject": "true",
+        }
+      },
     },
   }, {
     dependsOn: ns,
