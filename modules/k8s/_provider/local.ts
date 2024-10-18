ebea82d4cbeabda0277b481282f336153a071043
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-10-18 16:03:36 -0600

    local: minikube provider

--- modules/k8s/_provider/local.ts
+++ modules/k8s/_provider/local.ts
@@ -1,24 +1,13 @@
 import { local } from "@pulumi/command";
+import { MiniKube } from "../../minikube/provider";
 
 export default async function stack() {
-  const environment = {
-    KUBECONFIG: `${process.env["HOME"]}/.kube/local.config`,
-  };
-
-  const minikube = new local.Command("local-minikube", {
-    create: "minikube status || minikube start --cpus=2 --memory=8g --driver=docker --kubernetes-version=latest --insecure-registry=host.minikube.internal:5000",
-    delete: "minikube delete",
-    environment,
-  });
-  const metrics = new local.Command("local-metrics-server", {
-    create: "minikube addons enable metrics-server",
-  }, { dependsOn: [minikube]});
-  const runnit = new local.Command("local-kubeconfig", {
-    create: "cat ${KUBECONFIG}",
-    environment,
-  }, { dependsOn: [minikube, metrics] });
+  const minikube = new MiniKube("local-minikube", {
+    configPath: `${process.env["HOME"]}/.kube/local.config`,
+    addons: ["metrics-server"],
+  })
 
   return {
-    kubeconfig: runnit.stdout,
+    kubeconfig: minikube.kubeconfig,
   };
 }
