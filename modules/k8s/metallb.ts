b39e1d8792078efd78f3056a54b20b54f5feb7cd
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-11-10 14:03:37 -0700

    L2adverts and address pools

--- modules/k8s/metallb.ts
+++ modules/k8s/metallb.ts
@@ -1,5 +1,7 @@
 import * as k8s from "@pulumi/kubernetes";
 import { ModuleResultSet } from "./_basics";
+import { getStack } from "@pulumi/pulumi";
+import digitalocean from "../digitalocean";
 
 const namespace = "metallb-system";
 const version = "0.14.8";
@@ -25,9 +27,54 @@ export default async function metallbStack(provider: k8s.Provider, deployed: Mod
     },
   }, { provider, dependsOn: [ ns, ...(deployed.istio?.dependencies ?? []) ] });
 
+  const advertRes = new k8s.apiextensions.CustomResource("l2-advertisement", {
+    apiVersion: "metallb.io/v1beta1",
+    kind: "L2Advertisement",
+    metadata:{
+      name: "empty",
+      namespace,
+    }
+  }, {
+    provider,
+    dependsOn: [ ns, metallb ],
+  });
+
+  const poolRes = new k8s.apiextensions.CustomResource("addr-pool", {
+    apiVersion: "metallb.io/v1beta1",
+    kind: "IPAddressPool",
+    metadata:{
+      name: "addr-pool",
+      namespace,
+    },
+    spec: {
+      addresses: await getAddresses(),
+    },
+  }, {
+    provider,
+    dependsOn: [ ns, metallb, advertRes ],
+  });
+
   return {
     namespace: ns,
     releases: [ metallb ],
+    resources: [ advertRes, poolRes ],
     metallb,
+  }
+}
+
+async function getAddresses(): Promise<string[]> {
+  switch(getStack()) {
+    case "intranet":
+      return [ "192.168.68.24/32" ];
+    case "public": {
+      const { droplet } = await digitalocean(true);
+      
+      return [
+        `${droplet.ipv4Address}/32`,
+        `${droplet.ipv6Address}/128`,
+      ];
+    }
+    default:
+      throw new Error("stack not supported");
   }
 }
