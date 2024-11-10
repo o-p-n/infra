9363c1de697456ca00060274e05c60834bca6caf
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-11-10 09:17:50 -0700

    fixing up intranet ACME

--- modules/k8s/certificates/acme.ts
+++ modules/k8s/certificates/acme.ts
@@ -1,19 +1,21 @@
 import * as k8s from "@pulumi/kubernetes";
 import { Config } from "@pulumi/pulumi";
+import { namespace as certManagerNamespace } from "../cert-manager";
 
 const config = new Config();
 
 export function setupIssuer(provider: k8s.Provider) {
-  const creds = config.requireSecret("acme-creds");
+  const accessToken = config.requireSecret("acme-access-token");
   const email = config.require("acme-email");
 
   const resource = new k8s.core.v1.Secret("cert-manager-creds", {
     metadata: {
+      namespace: certManagerNamespace,
       name: "cert-manager-creds",
     },
     type: "Opaque",
     stringData: {
-      ACCESS_TOKEN: creds,
+      ACCESS_TOKEN: accessToken,
     },
   }, { provider });
 
