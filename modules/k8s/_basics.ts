841836140ffe723adffda17406b5ddf23ffc24a9
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-11-10 09:07:19 -0700

    allow for 'raw' resources in k8s modules

--- modules/k8s/_basics.ts
+++ modules/k8s/_basics.ts
@@ -2,14 +2,16 @@ import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
 export interface ModuleResult {
-  namespace?: k8s.core.v1.Namespace,
-  releases: k8s.helm.v3.Release[],
+  namespace?: k8s.core.v1.Namespace;
+  releases?: k8s.helm.v3.Release[];
+  resources?: pulumi.Resource[];
   dependencies?: pulumi.Resource[];
 }
 
 export interface ModuleOutput {
   namespace?: pulumi.Output<string>;
   releases?: pulumi.Output<string>[];
+  resources?: pulumi.Output<string>[];
 }
 
 function outputStack(input: ModuleResult) {
@@ -19,17 +21,21 @@ function outputStack(input: ModuleResult) {
     result.namespace = input.namespace.metadata.namespace;
   }
 
-  const releases = input.releases.map((rel) => {
+  const releases = input.releases?.map((rel) => {
     return pulumi.all([rel.repositoryOpts.repo, rel.name, rel.id]).apply(([repo, name]) => {
       return `${repo ?? ""}/${name}`;
     });
   });
   result.releases = releases;
 
+  const resources = input.resources?.map((res) => res.urn);
+  result.resources = resources;
+
   return result;
 }
 
 export type ModuleResultSet = Record<string, ModuleResult>;
+export type ModuleProvisioner = (provider: k8s.Provider, deployed: ModuleResultSet) => Promise<ModuleResult>;
 
 export class K8sModuleRegistry {
   provider: k8s.Provider;
@@ -39,12 +45,13 @@ export class K8sModuleRegistry {
     this.provider = provider;
   }
 
-  async apply(name: string, fn: (provider: k8s.Provider, deployed?: ModuleResultSet) => Promise<ModuleResult>) {
+  async apply(name: string, fn: ModuleProvisioner) {
     const input = await fn(this.provider, this.results);
     input.dependencies = [
       ...(input.dependencies ?? []),
       ...((input.namespace && [ input.namespace ]) ?? []),
-      ...input.releases,
+      ...(input.releases ?? []),
+      ...(input.resources ?? []),
     ];
     this.results[name] = input;
   }
