0b0e00425e3db62e800b15c3e7c5a0cda7c7162c
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-11-15 08:01:54 -0700

    fixed k8s version

--- modules/k8s/_provider/local.ts
+++ modules/k8s/_provider/local.ts
@@ -4,6 +4,7 @@ import { MiniKube } from "../../../providers/minikube";
 export default async function stack() {
   const minikube = new MiniKube("local-minikube", {
     configPath: `${process.env["HOME"]}/.kube/local.config`,
+    version: "1.31",
     addons: ["metrics-server"],
   })
 
