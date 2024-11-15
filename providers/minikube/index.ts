ecb074fcef74c5928453adcdf85b3a60b5bf4ab7
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-11-15 07:58:24 -0700

    improve MiniKube provider

--- providers/minikube/index.ts
+++ providers/minikube/index.ts
@@ -1,32 +1,84 @@
 import { $ } from "zx";
-import { randomBytes } from "crypto";
-import * as pulumi from "@pulumi/pulumi";
+import { randomBytes, subtle } from "node:crypto";
+import * as os from "node:os";
+import * as _ from "underscore";
 
-interface MiniKubeInputs {
+import { CustomResourceOptions, ID, Input, log, Output } from "@pulumi/pulumi";
+import * as dynamic from "@pulumi/pulumi/dynamic";
+
+interface ProviderInputs {
   configPath: string;
   version?: string;
   addons?: string[];
   cpus?: number;
   memory?: string;
 }
-interface MiniKubeOutputs {
+interface ProviderOutputs extends ProviderInputs {
   kubeconfig: string;
 };
 
-const DEFAULTS: Partial<MiniKubeInputs> = {
+const DEFAULTS: Partial<ProviderInputs> = {
   version: "stable",
   cpus: 2,
   memory: "8g",
 };
 
-const provider: pulumi.dynamic.ResourceProvider = {
-  async create(inputs: MiniKubeInputs): Promise<pulumi.dynamic.CreateResult<MiniKubeOutputs>> {
+async function makeId(props: Record<string, string>): Promise<string> {
+  const data = Buffer.from(JSON.stringify(props));
+  const digest = await subtle.digest("SHA-256", data);
+  return Buffer.from(digest).toString("hex").substring(0, 32);
+}
+
+class Provider implements dynamic.ResourceProvider {
+  async diff (id: ID, olds: ProviderOutputs, news: ProviderInputs): Promise<dynamic.DiffResult> {
+    // apply defaults
+    news = {
+      ...DEFAULTS,
+      ...news,
+    }
+    const replaces: string[] = [];
+    let changes = false;
+
+    // triggers replacement
+    if (olds.memory !== news.memory) {
+      replaces.push("memory");
+      changes = true;
+    }
+    if (olds.cpus !== news.cpus) {
+      replaces.push("cpus");
+      changes = true;
+    }
+    if (olds.version !== news.version) {
+      replaces.push("version");
+      changes = true;
+    }
+
+    // needs update
+    const oldAddons = olds.addons || [];
+    const newAddons = news.addons || [];
+    const disabling = _.difference(oldAddons, newAddons);
+    const enabling = _.difference(newAddons, oldAddons);
+    log.debug(`addons enabling=${enabling.join(",")}, disabling=${disabling.join(",")}`);
+    changes = changes || enabling.length > 0 || disabling.length > 0;
+
+    changes = changes || olds.configPath !== news.configPath;
+
+    return {
+      changes,
+      replaces,
+      deleteBeforeReplace: true,
+    }
+  }
+
+  async create(inputs: ProviderInputs): Promise<dynamic.CreateResult<ProviderOutputs>> {
     inputs = {
       ...DEFAULTS,
       ...inputs,
     };
 
-    const id = randomBytes(16).toString("hex");
+    const id = await makeId({
+      hostname: os.hostname(),      
+    });
     const $$ = $({
       env: {
         ...process.env,
@@ -54,10 +106,51 @@ const provider: pulumi.dynamic.ResourceProvider = {
 
     return {
       id,
-      outs: { kubeconfig },
+      outs: {
+        ...inputs,
+
+        kubeconfig,
+      },
     }
-  },
-  async delete(id: string, props: MiniKubeInputs) {
+  }
+
+  async update(id: ID, olds: ProviderOutputs, news: ProviderInputs): Promise<dynamic.UpdateResult<ProviderOutputs>> {
+    const $$ = $({
+      env: {
+        ...process.env,
+        KUBECONFIG: news.configPath,
+      },
+    });
+
+    // relocation configuration
+    if (olds.configPath !== news.configPath) {
+      await $$`cp ${olds.configPath} ${news.configPath}`;
+    }
+
+    // addons to disable
+    const oldAddons = olds.addons || [];
+    const newAddons = news.addons || [];
+    const disabled = _.difference(oldAddons, newAddons);
+    for (const addon of disabled) {
+      await $$`minikube addons disable ${addon}`;
+    }
+
+    const enabled = _.difference(newAddons, oldAddons);
+    for (const addon of enabled) {
+      await $$`minikube addons enable ${addon}`;
+    }
+    const kubeconfig = (await $$`minikube kubectl -- config view --raw`).toString();
+
+    return {
+      outs: {
+        ...olds,
+        ...news,
+        kubeconfig,
+      },
+    }
+  }
+
+  async delete(id: string, props: ProviderOutputs) {
     const $$ = $({
       env: {
         ...process.env,
@@ -66,20 +159,27 @@ const provider: pulumi.dynamic.ResourceProvider = {
     });
 
     await $$`minikube delete`;
-  },
+  }
 };
 
 
 interface MiniKubeArgs {
-  configPath: pulumi.Input<string>;
-  version?: pulumi.Input<string>;
-  addons?: pulumi.Input<string[]>;
+  configPath: Input<string>;
+  version?: Input<string>;
+  addons?: Input<string[]>;
+  cpus?: Input<number>;
+  memory?: Input<string>;
 }
 
-export class MiniKube extends pulumi.dynamic.Resource {
-  readonly kubeconfig!: pulumi.Output<string>;
+export class MiniKube extends dynamic.Resource {
+  readonly configPath!: Output<string>;
+  readonly version?: Output<string>;
+  readonly addons?: Output<string[]>;
+  readonly cpus?: Output<number>;
+  readonly memory?: Output<string>;
+  readonly kubeconfig!: Output<string>;
 
-  constructor(name: string, args: MiniKubeArgs, opts?: pulumi.CustomResourceOptions) {
-    super(provider, name, { kubeconfig: undefined, ...args }, opts);
+  constructor(name: string, args: MiniKubeArgs, opts?: CustomResourceOptions) {
+    super(new Provider(), name, { kubeconfig: undefined, ...args }, opts);
   }
 }
