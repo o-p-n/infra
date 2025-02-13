13a0455172935ed1cb5c9d29c5a9da50bb4c8281
Matthew A. Miller <linuxwolf@outer-planes.net>
2025-02-13 12:37:14 -0700

    chore: minor k8s improvements (#19)
    adding some quality-of-life improvements

--- modules/k8s/_provider/index.ts
+++ modules/k8s/_provider/index.ts
@@ -4,7 +4,11 @@ import { Output, getStack } from "@pulumi/pulumi";
 export async function getProvider() {
   const stack = getStack();
   const mod = await import(`./${stack}`);
-  const providerArgs = await mod.default() as k8s.ProviderArgs;
+  const modProviderArgs = await mod.default() as k8s.ProviderArgs;
+  const providerArgs: k8s.ProviderArgs = {
+    ...modProviderArgs,
+    deleteUnreachable: true,
+  };
 
-  return new k8s.Provider("k8s-provider", providerArgs);
+  return new k8s.Provider("k8s-provider", modProviderArgs);
 }
