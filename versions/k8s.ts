c011ae1b39af30a4f4bb04d648ebec1a2dd6a1b7
Matthew A. Miller <linuxwolf@outer-planes.net>
2025-02-18 09:22:48 -0700

    genericize versions

---
+++ versions/k8s.ts
@@ -0,0 +1,8 @@
+export const VERSION_MAJOR = "1";
+export const VERSION_MINOR = "31";
+export const VERSION_PATCH = "4";
+export const VERSION = `${VERSION_MAJOR}.${VERSION_MINOR}`;
+export const VERSION_FULL = `${VERSION}.${VERSION_PATCH}`;
+
+export const CHANNEL = "stable";
+export const VERSION_CHANNEL = `${VERSION}/${CHANNEL}`;
