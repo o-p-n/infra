b6542550978887a00873ead79ec349acdf87e734
Matthew A. Miller <linuxwolf@outer-planes.net>
2025-12-22 09:24:42 -0700

    chore: upgrade dependencies (#40)
    Upgraded the dependencies on Istio and Cloudfared

--- modules/k8s/istio-system/index.ts
+++ modules/k8s/istio-system/index.ts
@@ -1,7 +1,7 @@
 import * as k8s from "@pulumi/kubernetes";
 import { Config } from "@pulumi/pulumi";
 
-const version = "1.26.1";
+const version = "1.28.1";
 const namespace = "istio-system";
 const repositoryOpts = {
   repo: "https://istio-release.storage.googleapis.com/charts",
