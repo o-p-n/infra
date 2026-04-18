9e3d767b5fd6a0173146d47bb7d0e4080cad954e
Matthew A. Miller <linuxwolf@outer-planes.net>
2026-04-18 09:30:07 -0600

    refactor to single project

--- o-p-n.k8s/index.ts
+++ o-p-n.k8s/index.ts
@@ -1,14 +1,14 @@
 import { Config, Output } from "@pulumi/pulumi";
 
-import { K8sModuleRegistry } from "../../modules/k8s/_basics";
-import infraCoreStack from "../../modules/k8s/infra-core";
-import istioSystemStack from "../../modules/k8s/istio-system";
-import certManagerStack from "../../modules/k8s/cert-manager";
+import { K8sModuleRegistry } from "./modules/k8s/_basics";
+import infraCoreStack from "./modules/k8s/infra-core";
+import istioSystemStack from "./modules/k8s/istio-system";
+import certManagerStack from "./modules/k8s/cert-manager";
 
-import cloudflareStack, { Settings as CFSettings } from "../../modules/cloudflare";
-import publicIngressStack from "../../modules/k8s/public-ingress";
-import certificatesStack from "../../modules/k8s/certificates";
-import monitoringStack from "../../modules/k8s/monitoring";
+import cloudflareStack, { Settings as CFSettings } from "./modules/cloudflare";
+import publicIngressStack from "./modules/k8s/public-ingress";
+import certificatesStack from "./modules/k8s/certificates";
+import monitoringStack from "./modules/k8s/monitoring";
 
 const config = new Config("o-p-n");
 
