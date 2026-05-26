d2156a0c9d794e29fafa7cb9a8c0a1cf17aa221f
Matthew A. Miller <linuxwolf@outer-planes.net>
2026-05-26 17:06:47 -0600

    making kind-cluster work

--- foundation/local/run.sh
+++ foundation/local/run.sh
@@ -1,24 +1,16 @@
 #! /usr/bin/env bash
 
-set -euo pipefail
+CMD="$1"
+shift
 
-IMAGE="o-p-n/ansible:latest"
-PROJECT_DIR="$(pwd)"
-
-
-DOCKER_SSH_OPTS=""
-case $(uname -s) in
-  "Darwin")
-    DOCKER_SSH_OPTS="-v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent"
+case "${CMD}" in
+  "playbook")
+    ansible-playbook -i inventory "$@"
     ;;
-  "Linux")
-    DOCKER_SSH_OPTS="-v ${SSH_AUTH_SOCK}:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent"
+  "run")
+    ansible "$@"
+    ;;
+  "shell")
+    exec "${SHELL}" "$@"
     ;;
 esac
-
-docker run --rm -ti \
-  --network=host \
-  ${DOCKER_SSH_OPTS} \
-  -v "${PROJECT_DIR}:/workspace:rw" \
-  "${IMAGE}" \
-  "$@"
