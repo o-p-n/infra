4f416db1b2c7b3140e67e4edbb9b7648bbf7b7a0
Matthew A. Miller <linuxwolf@outer-planes.net>
2026-06-07 10:25:47 -0600

    update containerized to support docker internally

--- foundation/local/run.sh
+++ foundation/local/run.sh
@@ -1,13 +1,26 @@
 #! /usr/bin/env bash
 
-CMD="$1"
-shift
+set -euo pipefail
 
-case "${CMD}" in
-  "playbook")
-    ansible-playbook -i inventory "$@"
+ANSIBLE_REMOTE_USER="${ANSIBLE_REMOTE_USER:-${USER}}"
+
+HOST_OS="$(uname -s)"
+case "${HOST_OS}" in
+
+  "Darwin")
+    DOCKER_SSH_OPTS="-v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent" 
     ;;
-  "run")
-    ansible "$@"
+
+  *)
+    DOCKER_SSH_OPTS="-v ${SSH_AUTH_SOCK}:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent"
     ;;
+
 esac
+
+docker run --rm -ti \
+  --network=host \
+  ${DOCKER_SSH_OPTS:-} \
+  -v "${PWD}:/workspace" \
+  -v "/var/run/docker.sock:/var/run/docker.sock" \
+  -e "ANSIBLE_REMOTE_USER=${ANSIBLE_REMOTE_USER}" \
+  o-p-n/ansible:latest "$@"
