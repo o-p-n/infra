12ec90c7329ab22a2c9306739eb2bcc97ee892f7
Matthew A. Miller <linuxwolf@outer-planes.net>
2026-07-06 09:48:28 -0600

    refactor into role(s)

---
+++ foundation/public/run.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ANSIBLE_REMOTE_USER="${ANSIBLE_REMOTE_USER:-${USER}}"
+
+HOST_OS="$(uname -s)"
+case "${HOST_OS}" in
+
+  "Darwin")
+    DOCKER_SSH_OPTS="-v /run/host-services/ssh-auth.sock:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent" 
+    ;;
+
+  *)
+    DOCKER_SSH_OPTS="-v ${SSH_AUTH_SOCK}:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent"
+    ;;
+
+esac
+
+WORKING_TMPDIR=${SCRIPT_DIR}/../../tmp
+
+mkdir -p "${WORKING_TMPDIR}"
+
+docker run --rm -ti \
+  --network=host \
+  ${DOCKER_SSH_OPTS:-} \
+  -v "${PWD}:/workspace:ro" \
+  -v "${WORKING_TMPDIR}:/scratch:rw" \
+  -v "/var/run/docker.sock:/var/run/docker.sock" \
+  -e "ANSIBLE_LOCAL_TMP=/scratch" \
+  -e "ANSIBLE_REMOTE_USER=${ANSIBLE_REMOTE_USER}" \
+  -e "TMPDIR=/scratch" \
+  o-p-n/ansible:latest "$@"
